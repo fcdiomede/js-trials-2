a27f40edc2308b6edcc21cfba7c92f98e6ac29b0
Fernanda Diomede <fcdiomede@gmail.com>
2020-07-28 21:50:02 -0500

    updated array file functions

--- arrays.js
+++ arrays.js
@@ -2,18 +2,35 @@
 
 
 // 1. printIndices
+// Print each item in the list, followed by its index.
+
+//     The output should look like this:
+//         apple 0
+//         berry 1
+//         cherry 2
 function printIndices(items) {
   // Replace this with your code
+  for (const index in items) {
+    console.log(`${items[index]} ${index}`);
+  }
 }
 
 
 // 2. everyOtherItem
+// Print a list of every other item in `items`
 function everyOtherItem(items) {
-  // Replace this with your code
+  const result = [];
+  for (const index in items) {
+    if (index % 2 === 0) {
+      result.push(items[index]);
+    }
+  }
+  console.log(result);
 }
 
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
   // Replace this with your code
+  return items.sort((a,b) => b - a).slice(-n);
 }
